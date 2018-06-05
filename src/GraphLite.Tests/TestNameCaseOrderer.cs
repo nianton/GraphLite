c488204f65cdf29f0e246319f75dbec46a8bb9ec
Nikolaos Antoniou <nianton@gmail.com>
2018-06-05 09:42:11 +0300

    Custom test ordering attribute

--- src/GraphLite.Tests/TestNameCaseOrderer.cs
+++ src/GraphLite.Tests/TestNameCaseOrderer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit.Abstractions;
 
@@ -8,7 +9,39 @@ namespace GraphLite.Tests
     {
         public IEnumerable<TTestCase> OrderTestCases<TTestCase>(IEnumerable<TTestCase> testCases) where TTestCase : ITestCase
         {
-            return testCases.OrderBy(tc => tc.TestMethod.Method.Name);
+            return testCases.OrderBy(tc => tc.TestMethod.Method, new TestMethodComparer());
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class PriorityAttribute : Attribute
+    {
+        public PriorityAttribute(int priority)
+        {
+            Priority = priority;
+        }
+
+        public int Priority { get; }
+    }
+
+
+    public class TestMethodComparer : IComparer<IMethodInfo>
+    {
+        public int Compare(IMethodInfo x, IMethodInfo y)
+        {
+            var priorityX = x.GetCustomAttributes(typeof(PriorityAttribute))
+                .OfType<PriorityAttribute>()
+                .FirstOrDefault()?.Priority ?? 0;
+
+            var priorityY = y.GetCustomAttributes(typeof(PriorityAttribute))
+                .OfType<PriorityAttribute>()
+                .FirstOrDefault()?.Priority ?? 0;
+
+            var priorityCompareResult = priorityX.CompareTo(priorityY);
+
+            return priorityCompareResult == 0
+                ? x.Name.CompareTo(y.Name)
+                : priorityCompareResult;
         }
     }
 }
